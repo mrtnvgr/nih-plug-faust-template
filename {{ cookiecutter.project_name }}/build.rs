30ca8a36aa121a26dd63b75b424b81c552a7e36b
mrtnvgr <martynovegorOF@yandex.ru>
2023-04-15 18:39:09 +0700

    Beautify build.rs

--- {{ cookiecutter.project_name }}/build.rs
+++ {{ cookiecutter.project_name }}/build.rs
@@ -1,6 +1,4 @@
-use faust_build::build_dsp;
-
 fn main() {
     println!("cargo:rerun-if-changed=dsp");
-    build_dsp("dsp/{{ cookiecutter.dsp_filename }}.dsp");
+    faust_build::build_dsp("dsp/{{ cookiecutter.dsp_filename }}.dsp");
 }
